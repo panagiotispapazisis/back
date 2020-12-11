b1f02351d41788b54016fa656fb50be6fbaf57d7
panagiotispapazisis <panagiotis60@outlook.com>
2020-12-11 20:48:59 +0200

    create backend

---
+++ server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import bodyParser from "body-parser";
+import userRoutes from "./routes/users.js";
+const app = express();
+app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  next();
+});
+
+app.use("/users", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
+});
