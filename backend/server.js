130c8270f61d16dad9ad883dcf04bfa97a775b09
panagiotispapazisis <panagiotis60@outlook.com>
2020-12-11 21:09:52 +0200

    asdsa

--- backend/server.js
+++ backend/server.js
@@ -24,3 +24,12 @@ app.use(function (req, res, next) {
 });
 
 app.use("/users", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
+});
